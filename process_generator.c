adf173bf9b9ee7c7b709c4bb9e9f027b93c1a1e7
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-18 14:35:19 +0200

    Use validateArguments()

--- process_generator.c
+++ process_generator.c
@@ -7,7 +7,7 @@ Queue *queue;
 
 void clearResources(int);
 void readInputFile(char *inputPath);
-bool validateArguments(int *schedAlgo, int *quantum);
+bool validateArguments(int schedAlgo, int quantum);
 
 int safeToDestroyMsgQueue = 0;
 
@@ -19,7 +19,7 @@ int main(int argc, char * argv[])
     // 1. Read the input files.
     readInputFile("processes.txt");
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    int schedAlgo, quantum;
+    int schedAlgo, quantum = -1;
     printf("Choode Scheduling Algorithm:\n 1: HPF \n 2: SRTN \n 3: RR \n");
     scanf("%d", &schedAlgo);
     if(schedAlgo == 3) 
@@ -27,6 +27,7 @@ int main(int argc, char * argv[])
         printf("Enter RR quantum: ");
         scanf("%d", &quantum);
     }
+    validateArguments(schedAlgo, quantum);
     // 3. Initiate and create the scheduler and clock processes.
     int clk_pid = fork();
     if(clk_pid == 0) 
@@ -44,7 +45,7 @@ int main(int argc, char * argv[])
     {
         printf("Initiate the Scheduler"); 
         // #TODO HERE WE WILL SEND SOME ARGS TO THE SCHEDULER
-        execv("./scheduler.out", (char *[]){"./scheduler.out", NULL});
+        execv("./scheduler.out", (char *[]){"./scheduler.out", schedAlgo, quantum, NULL});
     }
     else 
     {
@@ -124,17 +125,17 @@ void readInputFile( char *path)
     }
     fclose(file);
 }
-bool validateArguments(int *schedAlgo, int *quantum)
+bool validateArguments(int schedAlgo, int quantum)
 {
-    if(*schedAlgo > 3 || *schedAlgo < 1) 
+    if(schedAlgo > 3 || schedAlgo < 1) 
     {
         printf("Please provide a valid SchedAlgo");
-        return 0;
+        exit(-1);
     }
-    if(*schedAlgo == 3 && *quantum < 0) 
+    if(schedAlgo == 3 && quantum < 0) 
     {
-         printf("Please provide a valid quantum for RR");
-        return 0;
+        printf("Please provide a valid quantum for RR");
+        exit(-1);
     }
     
     return 1;
