74bb7adc295d183dad37de619edc379f2c2e1aa4
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-18 21:24:40 +0200

    Implement process.c main while loop and sending signals to scheduler

--- process.c
+++ process.c
@@ -2,18 +2,44 @@
 
 /* Modify this file as needed*/
 int remainingtime;
-
+int remaining, quantum, currQuantum, time;
+int prvTime;
 int main(int agrc, char * argv[])
 {
     initClk();
-    
     //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
+    remainingtime = atoi(argv[1]);
+    quantum = atoi(argv[2]);
+    currQuantum =  quantum;
+    prvTime = getClk();
+
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        time = getClk();
+        if(time != prvTime) // time is passed by one 
+        {
+            remaining-=1;
+            if(currQuantum > 0)
+            {
+                currQuantum-=1;
+            }
+            if(!currQuantum && remaining!=0)
+            {
+                // TODO: Send  signal to scheduler stating that quantum is finised
+                kill(getppid(), SIGCHLD);
+                // TODO: raise stop signal
+                raise(SIGSTOP); 
+                // the scheduler will send SIGCONT to process 
+                // when the process recivies a SIGCONT signal
+                // it will need to continueu with an updated quantum
+                currQuantum = quantum;
+            }
+            time = getClk();
+        }
+        prvTime = time;
     }
-    
+    // TODO: Send signal to scheduler stating termination
+    kill(getppid(), SIGUSR1);
     destroyClk(false);
     
     return 0;
