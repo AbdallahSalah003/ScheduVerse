a1e9d9c7b2d12a0454d119f1ad536020e430df4a
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-27 00:55:06 +0300

    Refactor process.c and delete pcss.c

--- process.c
+++ process.c
@@ -1,51 +1,47 @@
 #include "headers.h"
 
-/* Modify this file as needed*/
+
+
+
+
 int remainingtime;
-int quantum;
-int min(int a, int b) {
-    if (a < b)
-        return a;
-    else
-        return b;
-}
-void processTime(int time);
+bool stop ; 
 int main(int agrc, char * argv[])
 {
     initClk();
-    //TODO it needs to get the remaining time from somewhere
-    remainingtime = atoi(argv[1]);
-    quantum = atoi(argv[2]);
-    printf("Process with remaining time : %d has started a quantum\n", remainingtime);
-    while (1)
-    {
-        int compare = min(remainingtime, quantum);
-        processTime(compare);
-        remainingtime -= compare;
+    stop = 0 ; 
+
+    remainingtime = atoi(argv[1]); 
+    
+    int ind = atoi(argv[2]);
+
+    int prvtime = getClk();
+    while (remainingtime)
+    {       
+        int time = getClk(); 
+        while (prvtime==time)
+        {
+            time = getClk() ; 
+        }
+        prvtime=time;
+      //  printf("current clk is : %d",getClk());
+       
+        remainingtime-- ; 
+        //  printf("Process %d has consumed sec and it's remaing time: %d\n",ind , remainingtime);
         if(remainingtime <= 0) 
         { 
             break;
         }
+        // this part will be edites scheduler will be responsible for that 
+        /*
         // TODO: Send  signal to scheduler stating that quantum is finised
         kill(getppid(), SIGRTMIN+1);
         // TODO: raise stop signal
-        printf("Process has finished a quantum with remaing time : %d\n", remainingtime);
-        raise(SIGSTOP); 
-        // the scheduler will send SIGCONT to process 
+        */
     }
     // TODO: Send signal to scheduler stating termination
     kill(getppid(), SIGRTMIN+2);
 
     destroyClk(false);    
     return 0;
-}
-void processTime(int time) {
-    int startTime = getClk();
-    int currTime;
-    while(time > 0) {
-        currTime = getClk();
-        if(startTime == currTime) continue;
-        startTime = getClk();
-        time--;
-    }
 }
